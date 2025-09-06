c28e38ae1827666b4800e4cf1618ae503fba3fea
Arif V <135023226+arif24v@users.noreply.github.com>
2025-09-05 20:07:28 -0400

    Create prototype0.js

---
+++ prototype0.js
@@ -0,0 +1,40 @@
+export const cards = [
+  {
+    "name": "Negative Side Effects",
+    "definition": "Occurs when the loss function is not properly specified such that in optimizing a given quantity, other major factors are ignored that should have been taken into acccount",
+    "failureMode": "The system performs actions that make the scenario worse overall, but optimize what they were asked to do.",
+    "example": "When an AI car burns through 2x as much fuel to save 5 minutes on a 2 hour road trip because it was told to get to the destination quickly."
+  },
+];
+export const cards = [
+  {
+    "name": "Reward Hacking",
+    "definition": "Occurs when the loss function is not properly specified, such that the system games the developer's intent without actually completing the task at hand",
+    "failureMode": "The system performs actions that were contrary to the developer's intent but technically follow the loss function's minimization",
+    "example": "When an AI racing model is told to cross the finish line of a circular track as fast as possible, but simply reverses the car without driving through the course to finish faster."  
+  },            
+];
+export const cards = [
+  {
+    "name": "Scalable Oversight",
+    "definition": "xxx",
+    "failureMode": "xxx",
+    "example": "xxx"
+  },
+];
+export const cards = [
+  {
+    "name": "Safe Exploration",
+    "definition": "xxx",
+    "failureMode": "xxx",
+    "example": "xxx"
+  },                                             
+];
+export const cards = [
+  {
+    "name": "Robustness to Distributional Shift",
+    "definition": "xxx",
+    "failureMode": "xxx",
+    "example": "xxx"
+  },                                             
+];
